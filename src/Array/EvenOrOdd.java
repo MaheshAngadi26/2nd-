50f1b6f6eaf7d411a68e20e96d87679bdbb98165
LENOVO <LENOVO@Mahesha>
2024-05-26 20:37:55 +0530

    Jai

---
+++ src/Array/EvenOrOdd.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class EvenOrOdd {
+
+	
+	//check wheather the given number is even or odd
+	public static void main(String[] args)
+	{
+		int a=40;
+		if(a%2==0)
+		{
+			System.out.println("a"+"is even number");
+		}
+		else
+		{
+			System.out.println("a"+"is odd number");
+		}
+
+	}
+
+}
